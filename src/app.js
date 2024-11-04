186e4faedcfbc79881447121b268d9d5647a0e5d
Jon Buggins <jonbuggins@gmail.com>
2024-11-04 15:49:08 +0000

    initial commit - up to part 3

--- src/app.js
+++ src/app.js
@@ -6,11 +6,16 @@ const { db } = require("../db/connection")
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+});
 
+app.get("/musicians/:id", async (req, res) => {
+    const id = req.params['id'];
+    const musician = await Musician.findByPk(id);
+    res.json(musician);
 
-
-
-
-
+});
 
 module.exports = app;
